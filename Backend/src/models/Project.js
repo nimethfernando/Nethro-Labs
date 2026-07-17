63ee21b314c8b9f1e0a687a96e92e05c4073404c
Nimeth Fernando <nimeth42@gmail.com>
2026-07-17 20:11:25 +0530

    refactor(Client Dashboard): Update file name
    File name was got updated

--- Backend/src/models/Project.js
+++ Backend/src/models/Project.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import User from '../models/User.js';
-import Project from '../models/Project.js';
+import User from './User.js';
+import Project from './Project.js';
 
 const router = express.Router();
 
